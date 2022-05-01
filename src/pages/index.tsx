f6030686e248245990ce03b064bb5d5bdba78a65
clepverse <cleepe5@hotmail.com>
2022-05-01 00:37:53 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | clepverse.news</title>
+        <title>Home | clepverse.news</title>
       </Head>
-      <div>
-        <h1>
-          Hello <span>World</span>{' '}
-        </h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+      </main>
     </>
   );
 }
